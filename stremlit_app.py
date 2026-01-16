5f39ffb6c8d53d224b9f3e81b27ce4258efb3575
hiki0900 <68093472+hiki0900@users.noreply.github.com>
2026-01-16 16:21:26 +0900

    Refactor Streamlit app to load model and display prediction
    Removed unused imports and Streamlit components. Added model loading and prediction display.

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,25 +1,10 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import altair as alt
+import joblib
 
-st.header('st.write')
+@st.cache_resource
+def load_model():
+  return joblib.load('model.pkl')
 
-#텍스트와 마크다운 형식의 텍스트 표시
-st.write('Hello, *World!* :sunglasses:')
-#숫자와 같은 다른 데이터 형식 표시
-st.write(1234)
-#Dataframe 표시
-df =  pd.DataFrame({
-    '첫번째 칼럼': [1, 2, 3, 4],
-    '두번째 칼럼': [10, 20, 30, 40]})
-st.write(df)
-#여러 인수 전달
-st.write('아래는 데이터 프레임이다:', df, '위에는 데이터 프레임이다.')
-
-df2 = pd.DataFrame(
-    np.random.randn(200, 3),
-    columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+model = load_model()
+prediction = model.prediction([[1,2,3]])
+st.write(prediction)
