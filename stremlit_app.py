4b4a2c5456fd6a5f535ddf365603293a5c649a64
hiki0900 <68093472+hiki0900@users.noreply.github.com>
2026-01-16 17:17:50 +0900

    Implement weight conversion between lbs and kg
    Removed model loading and prediction logic. Added functions to convert between pounds and kilograms with Streamlit input.

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,11 +1,18 @@
 import streamlit as st
-import joblib
-import pandas as pd
 
-@st.cache_resource
-def load_model():
-  return joblib.load('model.pkl')
+st.title("st.session_state")
 
-model = load_model()
-prediction = model.prediction([[1,2,3]])
-st.write(prediction)
+def lbs_to_kg():
+    st.session_state.kg = st.session_state.lbs/2.2046
+
+def kg_to_lbs():
+    st.session_state.lbs = st.session_state.kg*2.2046
+
+st.header("input")
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+    pounds =st.number_input("pound: ", on_change=lbs_to_kg, key="lbs")
+with col2:
+    kilogram = st.number_input("kilogram: ", on_change=kg_to_lbs, key="kg")
+st.header("output")
+st.write("st.session_state object: ", st.session_state)
